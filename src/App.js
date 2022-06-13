cacc70811df908a08ae6c9bbb9cb604fa2080181
Rengated <kaziev.sergey@matryoshka.studio>
2022-06-13 05:33:04 +0300

    adding navbar&homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Home from './pages/home';
+import { Header } from './components/Header/Header';
  
 
 
 function App() {
  
     return (
-        <Routes>
-              <Route path="/" element={<Home/>}/>          
-        </Routes>
+        <div className='container-xl'>  
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>          
+            </Routes>
+        </div>
+        
+ 
     )
 }
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import react from 'react'
+import { useState } from 'react'
+import '../../styles/header.css'
+
+
+export const Header = () => {
+    const [theme, setTheme] = useState('White')
+
+    const handleThemeClick = () => {
+        setTheme(theme == 'white' ? 'black' : 'white')
+    }
+
+
+    return (
+        <div className='container-xl d-flex justify-content-between header'>
+            <h1 className='header__title'>Where in the world ?</h1>
+            <button className='header__button' onClick={handleThemeClick}>{theme} mode</button>
+        </div>
+    )
+}
